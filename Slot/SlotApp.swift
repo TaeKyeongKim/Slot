4d73a355d625c7bfd51a8747f240598c74b47fb5
TaeKyeongKim <rlaxorud1324@naver.com>
2021-12-17 22:20:38 +0900

    Initial Commit

---
+++ Slot/SlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotApp.swift
+//  Slot
+//
+//  Created by Kim Tae Kyeong  on 2021/12/17.
+//
+
+import SwiftUI
+
+@main
+struct SlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
